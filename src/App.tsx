8b106c70a165b3153d83dc3fcd1a1f5543a2aeae
ricardo paulo <pauloricardo_wow@hotmail.com>
2023-03-25 18:37:13 -0300

    Ajuste lógica de renderização de paginação com hook useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ interface Posts {
 function App() {
   const [posts, setPosts] = useState<Posts[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     api
@@ -23,32 +23,19 @@ function App() {
       .then((response) => setPosts(response.data));
   }, []);
 
-  async function loadMorePosts() {
-    setCurrentPage((prevPage) => prevPage + 1);
-    setIsLoading(true);
-    const response = await api.get(`/posts?_page=${currentPage + 1}&_limit=10`);
-    setPosts((prevPost) => [...prevPost, ...response.data]);
-    setIsLoading(false);
-  }
-
   useEffect(() => {
-    function handleScroll() {
-      const { scrollTop, scrollHeight } = document.documentElement;
-      const windowHeight = window.innerHeight;
-      const scrollPosition = scrollTop + windowHeight;
-      const scrollBottom = scrollHeight - 50; // margem de erro de 50 pixels
-
-      if (scrollPosition >= scrollBottom) {
-        loadMorePosts();
-      }
+    if (currentPage === 1) {
+      return;
     }
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+    setLoading(true);
+    api
+      .get(`/posts?_page=${currentPage}&_limit=10`)
+      .then((response) => setPosts((prev) => [...prev, ...response.data]));
   }, [currentPage]);
+
+  function handlePost() {
+    setCurrentPage((prev) => prev + 1);
+  }
 
   return (
     <>
@@ -63,7 +50,8 @@ function App() {
             content={post.body}
           />
         ))}
-        {isLoading && <p>Loading...</p>}
+        <button onClick={handlePost}>Carregar mais posts</button>
+        {/* {loading && <p>Loading...</p>} */}
       </Wrapper>
       <GlobalStyle />
     </>
